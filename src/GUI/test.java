c34d3320925dbb5aed13fca731caca9ba658c70a
Sevastian-Bahynskyi <baginski.play@gmail.com>
2022-11-28 10:21:00 +0100

    2-nd commit

---
+++ src/GUI/test.java
@@ -0,0 +1,13 @@
+package GUI;
+
+import java.sql.SQLOutput;
+
+public class test
+{
+  public static void main(String[] args)
+  {
+    System.out.println("Hugo!!!!!!!!!!!");
+    System.out.println("Beeeee");//Kate//
+    System.out.println("Seva doesn't know");
+  }
+}
