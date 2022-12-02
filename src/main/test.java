cd7e973f480dde6a7001fabf019e0b65a6882380
00FA12 <331305@via.dk>
2022-12-02 13:40:07 +0100

    1

--- src/main/test.java
+++ src/main/test.java
@@ -8,5 +8,6 @@ public class test
   public static void main(String[] args)
   {
     Association association = AssociationModelManager.getAssociation();
+    System.out.println("Agua con pan");
   }
 }
